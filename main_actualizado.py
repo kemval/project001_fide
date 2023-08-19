cc1e550dba847c33f6a72a94d9dc2eb22db8a240
karlkissling <51515216+karlkissling@users.noreply.github.com>
2023-08-19 17:36:40 -0600

    Add files via upload

---
+++ main_actualizado.py
@@ -0,0 +1,71 @@
+import funciones
+
+funciones.cargar_datos_json()
+
+while True:
+
+    if not funciones.autenticado:  
+        print("\nMenu Principal:\n")
+        print("1. Registro")
+        print("2. Ingresar")
+        print("3. Salir\n")
+    
+        funciones.mostrar_registro()
+        opcion = input("\nElija una opción: ")
+        funciones.manejar_opciones_registro(opcion)
+
+    else:  
+        print("\nMenú Autenticado:\n")
+        print("1. Registro de nueva casa y habitacion")
+        print("2. Ver casas y habitaciones registradas")
+        print("3. Actualizar PIN")
+        print("4. Agregar un dispositivo")              #KISS & ERICK WORKING ON IT
+        print("5. Cerrar sesión\n")
+        
+
+        autenticado_opcion = input("Elija una opción: ")
+
+        if autenticado_opcion == "1":
+
+            funciones.registrar_casa_habitacion()
+            funciones.guardar_datos_json()
+
+        elif autenticado_opcion == "2":
+
+            print("\nCasas y Habitaciones Registradas:\n")
+
+            for casa, habitaciones in funciones.casas_creadas.items():
+
+                print(f"Casa: {casa}")
+
+                for habitacion in habitaciones['habitaciones']:
+                    print(f"Habitación: {habitacion}")
+
+                print("")  
+
+
+
+        elif autenticado_opcion == "3":
+
+            pinActual = input("\nIngrese su Contraseña Actual: ")
+            pinNuevo = input("\nIngrese su Nueva Contraseña:")
+            funciones.cambiarPin(pinActual, pinNuevo)
+            funciones.guardar_datos_json()
+
+
+
+        elif autenticado_opcion == "4":
+
+            funciones.AgregarDispositivo()         
+            funciones.guardar_datos_json()
+
+
+        elif autenticado_opcion == "5":
+
+            print("Cerrando sesión...")
+            funciones.usuario_autenticado = None  
+            funciones.autenticado= False
+            funciones.guardar_datos_json()
+
+        else:
+            print("Opcion invalida")        
